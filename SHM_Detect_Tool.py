40e994751ae364445ad729a080b92f215bf62446
Chao Zhou <zhouchao486@gmail.com>
2020-09-02 14:18:55 +0800

    create a shm detect function

--- SHM_Detect_Tool.py
+++ SHM_Detect_Tool.py
@@ -1,23 +1,26 @@
+# -*- coding: utf-8 -*-
+
 import torch
 from torch import nn
 from torch.nn import init
 import numpy as np
 import sys
+
 sys.path.append("..")
 import d2lzh_pytorch as d2l
 
 # %% load data
-batch_size = 12 #256
-train_iter, test_iter = d2l.load_custom_shm_data(batch_size) #d2l.load_data_fashion_mnist(batch_size)
+batch_size = 12  # 256
+train_iter, test_iter = d2l.load_custom_shm_data(batch_size)  # d2l.load_data_fashion_mnist(batch_size)
 
 # %% define&initial module
-num_inputs, num_outputs, num_hiddens = 121, 2, 64#784, 10, 256
+num_inputs, num_outputs, num_hiddens = 121, 2, 64  # 784, 10, 256
 net = nn.Sequential(
     d2l.FlattenLayer(),
     nn.Linear(num_inputs, num_hiddens),
     nn.ReLU(),
     nn.Linear(num_hiddens, num_outputs),
-    )
+)
 for params in net.parameters():
     init.normal_(params, mean=0, std=0.01)
 
@@ -33,7 +36,8 @@ d2l.train_ch3(net, train_iter, test_iter, loss, num_epochs, batch_size, None, No
 
 # %% show result
 X, y = iter(test_iter).next()
-true_labels = d2l.get_custom_shm_labels(y.numpy())#d2l.get_fashion_mnist_labels(y.numpy())
-pred_labels = d2l.get_custom_shm_labels(net(X).argmax(dim=1).numpy())#d2l.get_fashion_mnist_labels(net(X).argmax(dim=1).numpy())
+true_labels = d2l.get_custom_shm_labels(y.numpy())  # d2l.get_fashion_mnist_labels(y.numpy())
+pred_labels = d2l.get_custom_shm_labels(
+    net(X).argmax(dim=1).numpy())  # d2l.get_fashion_mnist_labels(net(X).argmax(dim=1).numpy())
 titles = [true + '\n' + pred for true, pred in zip(true_labels, pred_labels)]
 d2l.show_fashion_mnist(X[0:9], titles[0:9])
