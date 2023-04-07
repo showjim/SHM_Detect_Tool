6980f8a2c4f2aa9a7993b1623d86380c680188c9
Chao Zhou <chao.zhou@teradyne.com>
2023-04-07 16:56:38 +0800

    support to extract site index from default CHAR studio

--- SHM_keywords_setting.py
+++ SHM_keywords_setting.py
@@ -8,13 +8,13 @@
 keyword_site = 'Site'  # 'DEVICE_NUMBER:' #'Site:' #'DEVICE_NUMBER:'
 
 # the keyword to indicate the test name line
-keyword_item = 'Test Name'  # 'Test Name' #'_SHM:' #'TestSuite = '
+keyword_item = '<' #'Test Name'  # 'Test Name' #'_SHM:' #'TestSuite = '
 
 # the keyword to indicate the Shmoo plot start line
 keyword_start = 'Tcoef(AC Spec)'  # "Tcoef(AC Spec)" #'Tcoef(%)'
 
 # the keyword to indicate the Shmoo plot start line, but this is not used in the tool for now
-keyword_end = 'Tcoef(%)'
+keyword_end = 'X Axis: Vcoef(DC Spec)'#'Tcoef(%)'
 
 # the keyword to indicate the pass symbol in Shmoo plot, reg exp support
 keyword_pass = '\+'  # 'P|\*' #'\+'
